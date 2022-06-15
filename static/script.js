eeb394fb0238f88338dcdaf0cbdc9c6902f061cd
tyrkinn <nikemad61@gmail.com>
2022-06-15 11:07:19 +0300

    Update README.md

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.querySelector('button');
 toggleButton.addEventListener('click', async () => {
-  const toggleRestAddress = 'http://192.168.43.253:80/toggle';
-  const toggleRequest = await fetch(toggleRestAddress);
+  const toggleRestAddress = '/toggle';
+  await fetch(toggleRestAddress);
 })
